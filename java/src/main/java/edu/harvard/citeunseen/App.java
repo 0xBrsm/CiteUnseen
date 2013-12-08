380b5b5aa7a84776019a77502e82a37473dab928
Brian St. Marie <50640968+0xBrsm@users.noreply.github.com>
2013-12-08 12:53:15 -0500

    java: bootstrap Maven skeleton

---
+++ java/src/main/java/edu/harvard/citeunseen/App.java
@@ -0,0 +1,13 @@
+package edu.harvard.citeunseen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
